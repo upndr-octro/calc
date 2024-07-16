474f9bbdaa858e83a0cff1be1fb5bf547a109ba2
upndr-octro <upendra.bajpai@octrotalk.com>
2024-07-16 12:49:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Simple Calculator</title>
+    <title>Simple PHP Calculator</title>
     <style>
         body {
             font-family: Arial, sans-serif;
